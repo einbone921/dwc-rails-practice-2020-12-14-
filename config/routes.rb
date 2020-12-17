66923628c6c73c35172ce523422ded2201ceae42
einbone921 <einbone921@gmail.com>
2020-12-17 00:53:53 +0000

    [Add] usersコントローラの追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
-  get 'books/index'
-  get 'books/show'
-  get 'books/edit'
   devise_for :users
   root to: 'homes#top'
   get '/about' => 'homes#about'
-  resources :books, only: [:index, :show, :edit, :update, :destroy]
+  resources :books, only: [:index, :show, :create, :edit, :update, :destroy]
+  resources :users, only: [:index, :show, :create, :edit, :update, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
